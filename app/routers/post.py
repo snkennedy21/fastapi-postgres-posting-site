e7dd93aff930ef12f6afb988afa80d9004d8f9e1
Sean Kennedy <snkennedy21@gmail.com>
2022-11-21 09:46:39 -0500

    set up environment variables

---
+++ app/routers/post.py
@@ -0,0 +1,81 @@
+from fastapi import Response, status, HTTPException, Depends, APIRouter
+from .. import models, schemas, oauth2
+from typing import List, Optional
+from sqlalchemy.orm import Session
+from ..database import get_db
+
+router = APIRouter(
+  prefix='/posts',
+  tags=["Posts"]
+)
+
+
+@router.get("/", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ''):
+
+  posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+  return posts
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+  new_post = models.Post(owner_id=current_user.id, **post.dict())
+  db.add(new_post) # Adds the post to the database
+  db.commit() # Saves the added post to the database
+  db.refresh(new_post) # Returns the new post
+  return new_post 
+
+@router.get('/{id}', response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+  post = db.query(models.Post).filter(models.Post.id == id).first()
+  print(post)
+
+  if not post:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"post with id: {id} was not found"
+    )
+  return post
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT,)
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+  post_query = db.query(models.Post).filter(models.Post.id == id)
+
+  post = post_query.first()
+
+  if post == None:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Post with id {id} does not exist"
+    )
+
+  if post.owner_id != current_user.id:
+    raise HTTPException(
+      status_code=status.HTTP_403_FORBIDDEN,
+      detail="Not Authorized to perform requested action"
+    )
+
+  post_query.delete(synchronize_session=False)
+  db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put('/{id}', response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+  post_query = db.query(models.Post).filter(models.Post.id == id)
+  post = post_query.first()
+
+  if post == None:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Post with id {id} does not exist"
+    )
+
+  if post.owner_id != current_user.id:
+    raise HTTPException(
+      status_code=status.HTTP_403_FORBIDDEN,
+      detail="Not Authorized to perform requested action"
+    )
+
+  post_query.update(updated_post.dict(), synchronize_session=False) 
+  db.commit()
+  return post_query.first()
---
+++ app/schemas.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+
+class UserCreate(BaseModel):
+  email: EmailStr
+  password: str
+
+
+class UserOut(BaseModel):
+  id: int
+  email: EmailStr
+  created_at: datetime
+  
+  class Config:
+    orm_mode = True
+
+
+class UserLogin(BaseModel):
+  email: EmailStr
+  password: str
+
+class PostBase(BaseModel):
+  title: str
+  content: str
+  published: bool = True
+
+
+class PostCreate(PostBase):
+  pass
+
+
+class Post(PostBase):
+  id: int
+  created_at: datetime
+  owner_id: int
+  owner: UserOut
+
+  # Pydantic is typically only able to read dictionaries. This will enure that pydantic can read the ORM model returned by SQL Alchemy.
+  class Config:
+    orm_mode = True
+
+
+class Token(BaseModel):
+  access_token: str
+  token_type: str
+
+class TokenData(BaseModel):
+  id: Optional[str] = None
