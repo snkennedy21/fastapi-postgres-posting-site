9ab743b60bddbef05bdce8306d7580cdb530dada
Sean Kennedy <snkennedy21@gmail.com>
2022-11-21 18:23:20 -0500

    added test route

--- app/main.py
+++ app/main.py
@@ -23,3 +23,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get('/')
+def root():
+    return {"Message": "Hello World"}
