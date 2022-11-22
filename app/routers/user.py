a4a7dbc0270d557335e1909510eb0885b3bc6682
Sean Kennedy <snkennedy21@gmail.com>
2022-11-22 10:39:35 -0500

    add docker and docker-compose files for development and production

--- app/routers/user.py
+++ app/routers/user.py
@@ -11,12 +11,15 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+  print('hello')
  
   # Hash the password - user.password
   hashed_password = utils.hash(user.password)
   user.password = hashed_password
 
   new_user = models.User(**user.dict())
+
   db.add(new_user)
   db.commit()
   db.refresh(new_user)
