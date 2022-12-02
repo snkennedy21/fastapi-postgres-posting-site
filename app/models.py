2c6493e4246be3f70367dc6145b6d25a1e860987
Sean Kennedy <snkennedy21@gmail.com>
2022-12-02 11:23:17 -0500

    add username propert to user table and allow users to log in with username or email

--- app/models.py
+++ app/models.py
@@ -21,6 +21,7 @@ class User(Base):
   __tablename__ = "users"
 
   id = Column(Integer, primary_key=True, nullable=False)
+  username = Column(String, nullable=False, unique=True)
   email = Column(String, nullable=False, unique=True)
   password = Column(String, nullable=False)
   created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 
 from .. import database, schemas, models, utils, oauth2
 
@@ -11,8 +12,8 @@ router = APIRouter(
 
 @router.post('/login', response_model=schemas.Token)
 def login(response: Response, user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
-  
-  user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+
+  user = db.query(models.User).filter(or_(models.User.username == user_credentials.username, models.User.email == user_credentials.username)).first()
 
   if not user:
     raise HTTPException(
