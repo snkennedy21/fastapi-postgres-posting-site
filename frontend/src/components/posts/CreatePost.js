08d76f6ee7e06a436821a20988b6b64a1c153a82
Sean Kennedy <snkennedy21@gmail.com>
2022-12-06 11:31:20 -0500

    save

--- frontend/src/components/posts/CreatePost.js
+++ frontend/src/components/posts/CreatePost.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { useCreatePostMutation } from "../../store/rtk-query-apis/postsApi";
+import { useSelector } from "react-redux";
 
 function CreatePost() {
+  const token = useSelector((state) => state.token).token;
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [createPost] = useCreatePostMutation();
@@ -24,28 +26,32 @@ function CreatePost() {
     setContent(e.target.value);
   }
 
-  return (
-    <div className="flex flex-col justify-center items-center py-28">
-      <form
-        onSubmit={submitPostHandler}
-        className="bg-blue-100 flex flex-col gap-6 w-60 p-12"
-      >
-        <input
-          onChange={titleChangeHandler}
-          value={title}
-          name="title"
-          placeholder="Title"
-        ></input>
-        <textarea
-          onChange={contentChangeHandler}
-          value={content}
-          name="content"
-          placeholder="Content"
-        ></textarea>
-        <button className="bg-green-400">Post</button>
-      </form>
-    </div>
-  );
+  if (token) {
+    return (
+      <div className="flex flex-col justify-center items-center py-28">
+        <form
+          onSubmit={submitPostHandler}
+          className="bg-blue-100 flex flex-col gap-6 w-60 p-12"
+        >
+          <input
+            onChange={titleChangeHandler}
+            value={title}
+            name="title"
+            placeholder="Title"
+          ></input>
+          <textarea
+            onChange={contentChangeHandler}
+            value={content}
+            name="content"
+            placeholder="Content"
+          ></textarea>
+          <button className="bg-green-400">Post</button>
+        </form>
+      </div>
+    );
+  } else {
+    return <div>You are trying to access an unauthenticated route</div>;
+  }
 }
 
 export default CreatePost;
