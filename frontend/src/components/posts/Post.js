ef99f8f6337915cd771b623e6a104a6118cdd9a4
Sean Kennedy <snkennedy21@gmail.com>
2022-12-08 11:38:34 -0500

    refactor Post out of PostList

---
+++ frontend/src/components/posts/Post.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import {
+  useVoteMutation,
+  useDeleteVoteMutation,
+} from "../../store/rtk-query-apis/mainApi";
+
+function Post(props) {
+  const [addVote] = useVoteMutation();
+  const [deleteVote] = useDeleteVoteMutation();
+  const navigate = useNavigate();
+
+  function voteHandler(e) {
+    const postId = parseInt(e.target.dataset.post);
+    const voteDirection = parseInt(e.target.dataset.direction);
+    const upvote = e.target.dataset.upvote;
+    const postOwnedByCurrentUser = e.target.dataset.post_owned_by_current_user;
+    const addVoteData = {
+      post_id: postId,
+      direction: voteDirection,
+    };
+    const deleteVoteData = {
+      post_id: postId,
+    };
+
+    if (postOwnedByCurrentUser === "true") {
+      alert("Cannot Vote on Your Own Post");
+      return;
+    }
+
+    if (upvote === undefined) {
+      addVote(addVoteData);
+    } else if (upvote === "true" && voteDirection === 0) {
+      deleteVote(deleteVoteData);
+    } else if (upvote === "false" && voteDirection === 1) {
+      deleteVote(deleteVoteData);
+    }
+  }
+
+  function viewPostDetailHandler(e) {
+    const postId = parseInt(e.target.dataset.post);
+    navigate(`/posts/${postId}`);
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div>
+        <h2>Title: {props.postObj.Post.title}</h2>
+        <p>Content: {props.postObj.Post.content}</p>
+        <p>Owner: {props.postObj.Post.owner.username}</p>
+        <p>Votes: {props.postObj.upvotes - props.postObj.downvotes}</p>
+      </div>
+      <div>
+        <button
+          onClick={voteHandler}
+          data-post_owned_by_current_user={props.postObj.owner}
+          data-post={props.postObj.Post.id}
+          data-user_voted={props.postObj.user_voted}
+          data-upvote={props.postObj.upvote}
+          data-direction={1}
+          className="py-2 px-6 bg-green-400 text-2xl hover:bg-green-500 active:bg-green-600"
+        >
+          +
+        </button>
+        <button
+          onClick={voteHandler}
+          data-post_owned_by_current_user={props.postObj.owner}
+          data-post={props.postObj.Post.id}
+          data-user_voted={props.postObj.user_voted}
+          data-upvote={props.postObj.upvote}
+          data-direction={0}
+          className="py-2 px-6 bg-red-400 text-2xl hover:bg-red-500 active:bg-red-600"
+        >
+          -
+        </button>
+      </div>
+      <button
+        onClick={viewPostDetailHandler}
+        data-post={props.postObj.Post.id}
+        className="mt-2 py-2 px-6 bg-yellow-400 text-2xl hover:bg-yellow-500 active:bg-yellow-600"
+      >
+        View Details
+      </button>
+    </div>
+  );
+}
+
+export default Post;
--- frontend/src/components/posts/PostsList.js
+++ frontend/src/components/posts/PostsList.js
@@ -1,62 +1,20 @@
 // React Imports
 import React from "react";
 import { useState, useEffect } from "react";
-
-import { useNavigate } from "react-router-dom";
+import Post from "./Post";
 
 // Redux Imports
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 // RTK Query Imports
-import {
-  useGetAllPostsQuery,
-  useVoteMutation,
-  useDeleteVoteMutation,
-} from "../../store/rtk-query-apis/mainApi";
+import { useGetAllPostsQuery } from "../../store/rtk-query-apis/mainApi";
 
 function Posts() {
   const token = useSelector((state) => state.token).token;
   const { data: posts, isLoading } = useGetAllPostsQuery();
-  const [addVote] = useVoteMutation();
-  const [deleteVote] = useDeleteVoteMutation();
-  const navigate = useNavigate();
 
   if (isLoading) {
     return <div>Loading...</div>;
-  }
-
-  function voteHandler(e) {
-    const postId = parseInt(e.target.dataset.post);
-    const voteDirection = parseInt(e.target.dataset.direction);
-    const userVoted = e.target.dataset.user_voted;
-    const upvote = e.target.dataset.upvote;
-    const postOwnedByCurrentUser = e.target.dataset.post_owned_by_current_user;
-    const voteData = {
-      post_id: postId,
-      direction: voteDirection,
-    };
-    const deleteVoteData = {
-      post_id: postId,
-    };
-
-    if (postOwnedByCurrentUser === "true") {
-      alert("Cannot Vote on Your Own Post");
-      return;
-    }
-
-    if (upvote === undefined) {
-      addVote(voteData);
-    } else if (upvote === "true" && voteDirection === 0) {
-      deleteVote(deleteVoteData);
-    } else if (upvote === "false" && voteDirection === 1) {
-      deleteVote(deleteVoteData);
-    }
-  }
-
-  function viewPostDetailHandler(e) {
-    const postId = parseInt(e.target.dataset.post);
-    console.log(postId);
-    navigate(`/posts/${postId}`);
   }
 
   console.log(posts);
@@ -64,48 +22,8 @@ function Posts() {
   if (token) {
     return (
       <div className="mt-10 flex gap-12 justify-center items-center h-32 pt-10">
-        {posts.map((post) => {
-          return (
-            <div key={post.Post.id} className="flex flex-col">
-              <div>
-                <h2>Title: {post.Post.title}</h2>
-                <p>Content: {post.Post.content}</p>
-                <p>Owner: {post.Post.owner.username}</p>
-                <p>Votes: {post.upvotes - post.downvotes}</p>
-              </div>
-              <div>
-                <button
-                  onClick={voteHandler}
-                  data-post_owned_by_current_user={post.owner}
-                  data-post={post.Post.id}
-                  data-user_voted={post.user_voted}
-                  data-upvote={post.upvote}
-                  data-direction={1}
-                  className="py-2 px-6 bg-green-400 text-2xl hover:bg-green-500 active:bg-green-600"
-                >
-                  +
-                </button>
-                <button
-                  onClick={voteHandler}
-                  data-post_owned_by_current_user={post.owner}
-                  data-post={post.Post.id}
-                  data-user_voted={post.user_voted}
-                  data-upvote={post.upvote}
-                  data-direction={0}
-                  className="py-2 px-6 bg-red-400 text-2xl hover:bg-red-500 active:bg-red-600"
-                >
-                  -
-                </button>
-              </div>
-              <button
-                onClick={viewPostDetailHandler}
-                data-post={post.Post.id}
-                className="mt-2 py-2 px-6 bg-yellow-400 text-2xl hover:bg-yellow-500 active:bg-yellow-600"
-              >
-                View Details
-              </button>
-            </div>
-          );
+        {posts.map((postObj) => {
+          return <Post key={postObj.Post.id} postObj={postObj} />;
         })}
       </div>
     );
