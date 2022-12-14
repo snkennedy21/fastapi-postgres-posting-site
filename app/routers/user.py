7ad27a46db4c6cd2175b8b264cdb9ab7575984ec
Sean Kennedy <snkennedy21@gmail.com>
2022-12-14 13:35:57 -0500

    uncomment comments in create_user endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -33,17 +33,16 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
       detail=f"email {user.email} already exists"
     )
 
-  # Hash the password - user.password
-  # hashed_password = utils.hash(user.password)
-  # user.password = hashed_password
+  hashed_password = utils.hash(user.password)
+  user.password = hashed_password
 
-  # new_user = models.User(**user.dict())
+  new_user = models.User(**user.dict())
 
-  # db.add(new_user)
-  # db.commit()
-  # db.refresh(new_user)
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
 
-  # return new_user
+  return new_user
 
 @router.get('/{id}', response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
