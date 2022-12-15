3eb5e4191e47c88a019ca699143e49c0ce1e1270
Sean Kennedy <snkennedy21@gmail.com>
2022-12-15 17:00:04 -0500

    save

--- frontend/src/components/posts/post_detail/Comment.js
+++ frontend/src/components/posts/post_detail/Comment.js
@@ -2,16 +2,18 @@ import React from "react";
 
 function Comment(props) {
   return (
-    <div key={props.commentObj.Comment.id}>
-      <p>Comment: {props.commentObj.Comment.content}</p>
-      <p>Posted by: {props.commentObj.Comment.owner.username}</p>
-      {props.commentObj.owned_by_current_user ? (
-        <button className="bg-red-400 text-2xl hover:bg-red-500 active:bg-red-600">
-          Delete
-        </button>
-      ) : (
-        <></>
-      )}
+    <div className="flex flex-col">
+      <div className="flex flex-col px-2 justify-between">
+        <p>Comment: {props.commentObj.Comment.content}</p>
+        <p>Posted by: {props.commentObj.Comment.owner.username}</p>
+        {props.commentObj.owned_by_current_user ? (
+          <button className="bg-red-400 text-2xl hover:bg-red-500 active:bg-red-600">
+            Delete
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
