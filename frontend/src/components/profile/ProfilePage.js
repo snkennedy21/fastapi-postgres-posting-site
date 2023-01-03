6ef0e00d49af7c1fdf4e3947ec5f3248f66abd64
Sean Kennedy <snkennedy21@gmail.com>
2023-01-03 13:25:42 -0500

    fix image resize on profile page

--- frontend/src/components/profile/ProfilePage.js
+++ frontend/src/components/profile/ProfilePage.js
@@ -11,28 +11,33 @@ function ProfilePage() {
   }
 
   return (
-    <Container>
-      <div className="bg-lightBackground p-7 flex rounded-md">
-        <div className="pr-7 w-1/3">
-          <img className="rounded-full" src={profile} alt="profile picture" />
-        </div>
-        <div className="w-2/3">
-          <p className="text-2xl">
-            <span className="font-bold">Username:</span> {user.username}
-          </p>
-          <p className="text-2xl">
-            <span className="font-bold">Email:</span> {user.email}
-          </p>
-          <p className="text-2xl">
-            <span className="font-bold">About:</span> My name is sean kenedy and
-            I love to design posts for online forums. I hope you take the time
-            to read this and consider me My name is sean kenedy and I love to
-            design posts for online forums. I hope you take the time to read
-            this and consider me
-          </p>
+    <div className="flex justify-center">
+      <div className="p-10 flex flex-col gap-4 w-[1100px]">
+        <div className="bg-lightBackground p-7 flex rounded-md">
+          <div className="pr-7">
+            <img
+              className="rounded-full w-full"
+              src={profile}
+              alt="profile picture"
+            />
+          </div>
+          <div className="w-11/12">
+            <p className="text-2xl">
+              <span className="font-bold">Username:</span> {user.username}
+            </p>
+            <p className="text-2xl">
+              <span className="font-bold">Email:</span> {user.email}
+            </p>
+            <p className="text-2xl">
+              <span className="font-bold">About:</span> My name is sean kenedy
+              and I love to design posts for online forums. I hope you take the
+              time to read this and consider me My name is sean kenedy and I
+              love to design posts for online forums.
+            </p>
+          </div>
         </div>
       </div>
-    </Container>
+    </div>
   );
 }
 
