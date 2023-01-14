4d2d12b414873d05642d637af523325c008bac57
Sean Kennedy <snkennedy21@gmail.com>
2023-01-14 15:24:47 -0500

    fix heroku postgres tables

--- app/database.py
+++ app/database.py
@@ -10,6 +10,7 @@ from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
