de9f31a8dd605348ac35165748d494dcb6c56058
Sean Kennedy <snkennedy21@gmail.com>
2023-01-20 10:16:31 -0500

    change how cookies are delete on backend

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
+from datetime import datetime, timedelta
 
 from .. import database, schemas, models, utils, oauth2
 
@@ -30,13 +31,12 @@ def login(response: Response, user_credentials: OAuth2PasswordRequestForm = Depe
   access_token = oauth2.create_access_token(data={"user_id": user.id})
 
   response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True, expires=3600, secure=True, samesite="none")
-  # response.set_cookie(key="session", value="true", expires=3600, secure=True, samesite="none")
   return {"access_token": access_token, "token_type": "bearer"}
 
 @router.delete('/logout')
 def logout(response: Response):
-  response.delete_cookie(key="access_token")
-  response.delete_cookie(key="auth")
+  response.set_cookie(key="access_token", value="", expires=-1, httponly=True, secure=True, samesite="none")
+  response.set_cookie(key="session", value="", expires=-1, secure=True, samesite="none")
   response.status_code=status.HTTP_204_NO_CONTENT
   return response.status_code
   
